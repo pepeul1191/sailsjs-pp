d3b1712bd7d3e466e9c7cbff2fd40c866cbbfb70
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-07 14:14:16 -0500

    Primer controlador

---
+++ api/controllers/LoginController.js
@@ -0,0 +1,8 @@
+module.exports = {
+  index: function (req, res) {
+    return res.send('Hi there');
+  },
+  bye: function (req, res) {
+    return res.redirect('http://www.sayonara.com');
+  }
+};
--- config/autoreload.js
+++ config/autoreload.js
@@ -5,7 +5,8 @@ module.exports.autoreload = {
     "api/models",
     "api/controllers",
     "api/services",
-    "config/locales"
+    "config/locales",
+    "config",
   ],
   ignored: [
     // Ignore all files with .ts extension
