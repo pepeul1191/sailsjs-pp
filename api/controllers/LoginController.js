865eba1e8eed3c9225dd318e4bfef508ea60f6aa
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-08 08:57:02 -0500

    Pasando datos a la vista ejs desde controlador

--- api/controllers/LoginController.js
+++ api/controllers/LoginController.js
@@ -2,8 +2,8 @@ var unirest = require('unirest');
 
 module.exports = {
   index: function (req, res) {
-    var locals = {};
-    return res.view('login/index', locals);;
+    var locals = {'title': 'Bienvenido', 'mensaje': false};
+    return res.view('login/index', locals);
   },
   acceder: function (req, res) {
     unirest.post(sails.config.globals.servicios['accesos'] + 'usuario/acceder?usuario=' + req.param('usuario') + '&contrasenia=' + req.param('contrasenia'))
@@ -13,7 +13,8 @@ module.exports = {
         if (response.body == 1){
           res.redirect(sails.config.globals.base_url);
         }else{
-          return res.send('usuario y contraseña no válidos');
+          var locals = {'title': 'Bienvenido', 'mensaje': true};
+          return res.view('login/index', locals);
         }
       });
   },
